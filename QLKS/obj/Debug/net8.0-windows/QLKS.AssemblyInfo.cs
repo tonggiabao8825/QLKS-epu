cd55926da1684e7db7b4494bb87752514e3768eb
tonggiabao8825 <tonggiabao.media@gmail.com>
2025-11-15 20:55:59 +0700

    Init

--- QLKS/obj/Debug/net8.0-windows/QLKS.AssemblyInfo.cs
+++ QLKS/obj/Debug/net8.0-windows/QLKS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLKS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4e43e60d2aa95e87e379177713b78ed4cf36560")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7590e2438c30f92ad2d608e1fbf0b988c09085")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLKS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLKS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
